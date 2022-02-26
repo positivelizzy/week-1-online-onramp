660f9b2143cc534b78dd13d39ff2f3a565fe658a
positivelizzy <55453747+positivelizzy@users.noreply.github.com>
2022-02-26 14:04:39 +0100

    challenge 4 solution

--- algorithms/challenge-4.js
+++ algorithms/challenge-4.js
@@ -1,3 +1,20 @@
-function highestSum(x, y) {}
+function highestSum(x, y) {
+  var sum1 = arraySum(x);
+  var sum2 = arraySum(y);
+  if(sum1 > sum2 ){
+    return sum1;
+  }else {
+    return sum2;
+  }
+}
+
+// sum all elements in an array
+function arraySum(a){ 
+  let sum = 0;
+  for(var i in a){
+    sum+=i;
+  }
+  return sum;
+}
 
 module.exports = highestSum
