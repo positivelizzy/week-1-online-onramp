f341978c26df525d12674020aae8ff1a1bb6aba1
positivelizzy <55453747+positivelizzy@users.noreply.github.com>
2022-02-26 13:59:24 +0100

    chanllenge 2 solution

--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,3 +1,11 @@
-function divisible(a) {}
+function divisible(a) {
+  var number =0;
+  for (var i in a){
+    if((i % 2 === 0) && i > 15 ){
+      number++;
+    }
+  }
+  return number;
+}
 
 module.exports = divisible
